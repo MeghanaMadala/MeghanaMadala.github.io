fd723211cd8559b34655b5b18da88c477cef16fb
Meghana Madala <1313meghana@gmail.com>
2020-07-25 10:36:38 +0530

    React thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbIte
     Button, Modal, ModalHeader, ModalBody, Label, Row, Col } from 'reactstrap';
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -160,7 +161,26 @@ export class CommentForm extends Component {
 
     const DishDetail = (props) => {
         const dish = props.dish
-        if (props.dish == null) {
+        if(props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        
+        else if (props.dish == null) {
             return (<div></div>)
         }
         const dishItem = <RenderDish dish={props.dish} />
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,25 @@ export const addComment = (dishId, rating, author, comment) => ({
         author: author,
         comment: comment
     }
+});
+export const fetchDishes = () => (dispatch) => { /* The fetchDishes as you see is a punk which returns a function which when called will try to fetch the dishes first by setting the isLoading to "True", and so that the spinner can be displayed and after a period of time the dishes are fetched and then added into the application. So, that is what this fetchDishes is going to show for us. */
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000);
+}
+
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
 });
