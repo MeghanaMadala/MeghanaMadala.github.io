76a04f77e96b12d337a5fd6e2b0ad996498ff330
Meghana Madala <1313meghana@gmail.com>
2020-07-27 11:20:55 +0530

    fetch from server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -13,9 +14,9 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => { /* The fetchDishes as you see is a punk which returns a function which when called will try to fetch the dishes first by setting the isLoading to "True", and so that the spinner can be displayed and after a period of time the dishes are fetched and then added into the application. So, that is what this fetchDishes is going to show for us. */
     dispatch(dishesLoading(true));
 
-    setTimeout(() => {
-        dispatch(addDishes(DISHES));
-    }, 2000);
+    return fetch(baseUrl + 'dishes')
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)));
 }
 
 export const dishesLoading = () => ({
@@ -30,4 +31,43 @@ export const dishesFailed = (errmess) => ({
 export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
+});
+
+export const fetchComments = () => (dispatch) => { /* The fetchDishes as you see is a punk which returns a function which when called will try to fetch the dishes first by setting the isLoading to "True", and so that the spinner can be displayed and after a period of time the dishes are fetched and then added into the application. So, that is what this fetchDishes is going to show for us. */
+
+return fetch(baseUrl + 'comments')
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)));
+}
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromos = () => (dispatch) => { /* The fetchDishes as you see is a punk which returns a function which when called will try to fetch the dishes first by setting the isLoading to "True", and so that the spinner can be displayed and after a period of time the dishes are fetched and then added into the application. So, that is what this fetchDishes is going to show for us. */
+    dispatch(promosLoading(true));
+
+    return fetch(baseUrl + 'promotions')
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)));
+}
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
 });
