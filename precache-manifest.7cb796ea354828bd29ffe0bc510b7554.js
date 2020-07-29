7145370f0d32caf73f4261a131709987193108d4
Meghana Madala <1313meghana@gmail.com>
2020-07-29 19:35:55 +0530

    Updates

---
+++ precache-manifest.7cb796ea354828bd29ffe0bc510b7554.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8cb852eb7749ba3cdb60e556a1758f",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/index.html"
+  },
+  {
+    "revision": "85d6ba86881edd3a1659",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/css/2.2cf4ebac.chunk.css"
+  },
+  {
+    "revision": "c3f5290a8eea8cff7414",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/css/main.8728dafe.chunk.css"
+  },
+  {
+    "revision": "85d6ba86881edd3a1659",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/js/2.90ceaafc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/js/2.90ceaafc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f5290a8eea8cff7414",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/js/main.2bc632da.chunk.js"
+  },
+  {
+    "revision": "0aa89162782289bb6924",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/js/runtime-main.1c63f522.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MeghanaMadala/React-Deploy-Confusion.git/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
